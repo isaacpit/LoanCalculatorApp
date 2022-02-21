768919b3eb2da66c26b9ea574c2eaea38dbfad50
isaacpitblado <isaacpitblado@tamu.edu>
2022-02-21 15:39:30 -0600

    Initial Commit

---
+++ MortgageCalculator/MortgageCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MortgageCalculatorApp.swift
+//  MortgageCalculator
+//
+//  Created by Isaac Pitblado on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MortgageCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
